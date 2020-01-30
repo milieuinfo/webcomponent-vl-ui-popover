04daaca0ae988ffd61c43b83836fd1cfe4ba1f45
Pascal Riquier <pascal.riquier@vlaanderen.be>
2020-01-30 10:37:52 +0100

    IS-843

--- vl-popover.src.js
+++ vl-popover.src.js
@@ -1,19 +1,17 @@
-import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+import { NativeVlElement, define, awaitScript } from '/node_modules/vl-ui-core/vl-core.js';
 
-/**
- * VlPopover
- * @class
- * @classdesc 
- * 
- * @extends VlElement
- * 
- * @property 
- * 
- * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-popover/releases/latest|Release notes}
- * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-popover/issues|Issues}
- * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-popover.html|Demo}
- * 
- */
-export class VlPopover extends VlElement(HTMLElement) {}
+export class VlPopover extends NativeVlElement(HTMLDivElement) {
+	
+    get _stylePath() {
+        return '../style.css';
+    }
 
-define('vl-popover', VlPopover);
+}
+
+Promise.all([
+	  awaitScript('util', '/node_modules/@govflanders/vl-ui-util/dist/js/util.min.js'),
+	  awaitScript('core', '/node_modules/@govflanders/vl-ui-core/dist/js/core.min.js'),
+	  awaitScript('popover', '/node_modules/@govflanders/vl-ui-popover/dist/js/popover.js')
+]).then(() => define('vl-popover', VlPopover, {extends: 'div'}));
+
+
